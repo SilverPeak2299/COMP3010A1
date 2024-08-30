347eacda4299a859e0b3c1df56d69895d07c258c
SilverPeak2299 <dannydickerson03@gmail.com>
2024-08-30 11:39:51 +1000

    initial commit

---
+++ Graph.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Graph {
+    int[][] adjMatrix;
+
+    Graph(int noOfChem) {
+        adjMatrix = new int[noOfChem][noOfChem];
+    }
+
+    void generateMatrix(Scanner sc,int noOfGroups) {
+        
+    }
+}
---
+++ ass1_comp3010.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ass1_comp3010 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of chemicals to manage:");
+        int noOfChem = scanner.nextInt();
+
+        System.out.println("Enter the number of storage rooms:");
+        int noOfRooms = scanner.nextInt();
+
+        System.out.println("Enter the number of groups of incompatible chemicals you must separate:");
+        int noOfGroups = scanner.nextInt();
+
+
+
+    }
+}
